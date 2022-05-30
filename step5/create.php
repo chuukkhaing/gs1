c5b2bf9f103495944b1a0bab71bf01051f492d18
SkuzzyTop10 <106507214+SkuzzyTop10@users.noreply.github.com>
2022-05-30 13:55:56 +0630

    Update create.php

--- step5/create.php
+++ step5/create.php
@@ -16,10 +16,26 @@
     $gtins_price = $_POST['gtins_price'];
     if($gtins_quantity != '' && $gtins_year != '' && $gtins_price != '')
     {
-      $sql_order_receive_create = mysqli_query($con, "INSERT INTO order_receive (code, company_code, ticket_code, quantity, year, price, status) values('$order_code', '$company_code', '$ticket_code', '$gtins_quantity', '$gtins_year', '$gtins_price', '')")or die(mysqli_error($con));
-      if(isset($sql_order_receive_create))
+      //check member
+      $sql_company_information = mysqli_query($con, "SELECT * FROM company_information where company_code='$company_code' and ticket_code='$ticket_code'")or die(mysqli_error($con));
+      $sql_company_information_get = mysqli_fetch_object($sql_company_information);
+      //member
+      $member = $sql_company_information_get->member;
+      //price check
+      $sql_gtins = mysqli_query($con, "SELECT * FROM gtins where quantity='$gtins_quantity' and year='$gtins_year' and member='$member'")or die(mysqli_error($con));
+      $sql_gtins_get = mysqli_fetch_object($sql_gtins);
+      $sql_price = $sql_gtins_get->price;
+      if($gtins_price == $sql_price)
       {
-        echo "created";
+        $sql_order_receive_create = mysqli_query($con, "INSERT INTO order_receive (code, company_code, ticket_code, quantity, year, price, status) values('$order_code', '$company_code', '$ticket_code', '$gtins_quantity', '$gtins_year', '$gtins_price', '')")or die(mysqli_error($con));
+        if(isset($sql_order_receive_create))
+        {
+          echo "order created";
+        }
+      }
+      if($gtins_price != $sql_price)
+      {
+        echo "price wrong";
       }
     }
   }
